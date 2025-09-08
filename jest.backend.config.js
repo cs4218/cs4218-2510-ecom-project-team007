f941306776ad317ed929ab83c688cc95208e69f1
Gra7ityIC3 <kwtg20@outlook.com>
2025-09-08 12:11:12 +0800

    Update Jest config for frontend and backend

--- jest.backend.config.js
+++ jest.backend.config.js
@@ -5,12 +5,17 @@ export default {
   // when testing backend
   testEnvironment: "node",
 
-  // which test to run
-  testMatch: ["<rootDir>/controllers/*.test.js"],
+  // which tests to run
+  testMatch: [
+    "<rootDir>/config/*.test.js",
+    "<rootDir>/controllers/*.test.js",
+    "<rootDir>/helpers/*.test.js",
+    "<rootDir>/middlewares/*.test.js",
+    "<rootDir>/models/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["controllers/**"],
   coverageThreshold: {
     global: {
       lines: 100,
--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,15 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: [
+    "<rootDir>/client/src/components/**/*.test.js",
+    "<rootDir>/client/src/context/**/*.test.js",
+    "<rootDir>/client/src/hooks/**/*.test.js",
+    "<rootDir>/client/src/pages/**/*.test.js",
+  ],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
   coverageThreshold: {
     global: {
       lines: 100,
