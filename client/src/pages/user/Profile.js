75594a37261fc6af050fba643a4e075df9b59d22
yunruu <yunruuchin@gmail.com>
2025-10-19 23:25:35 +0800

    fix: remove duplicate form field id

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -66,7 +66,7 @@ const Profile = () => {
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     className="form-control"
-                    id="exampleInputEmail1"
+                    id="inputName"
                     placeholder="Enter Your Name"
                     autoFocus
                   />
@@ -77,7 +77,7 @@ const Profile = () => {
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                     className="form-control"
-                    id="exampleInputEmail1"
+                    id="inputEmail"
                     placeholder="Enter Your Email "
                     disabled
                   />
@@ -88,7 +88,7 @@ const Profile = () => {
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                     className="form-control"
-                    id="exampleInputPassword1"
+                    id="inputPassword"
                     placeholder="Enter Your Password"
                   />
                 </div>
@@ -98,7 +98,7 @@ const Profile = () => {
                     value={phone}
                     onChange={(e) => setPhone(e.target.value)}
                     className="form-control"
-                    id="exampleInputEmail1"
+                    id="inputPhone"
                     placeholder="Enter Your Phone"
                   />
                 </div>
@@ -108,7 +108,7 @@ const Profile = () => {
                     value={address}
                     onChange={(e) => setAddress(e.target.value)}
                     className="form-control"
-                    id="exampleInputEmail1"
+                    id="inputAddress"
                     placeholder="Enter Your Address"
                   />
                 </div>
