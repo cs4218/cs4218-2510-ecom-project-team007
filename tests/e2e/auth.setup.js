d754eb6ac560a3829a00855c0460107c88e1789a
hweinian <97207202+hweinian@users.noreply.github.com>
2025-10-21 17:24:02 +0800

    edit auth.setup

--- tests/e2e/auth.setup.js
+++ tests/e2e/auth.setup.js
@@ -5,8 +5,8 @@ const authFile = 'tests/e2e/.auth/user.json';
 // https://playwright.dev/docs/auth#basic-shared-account-in-all-tests
 setup('authenticate', async ({ page }) => {
   await page.goto('/login');
-  await page.getByPlaceholder('Enter your email').fill('test@admin.com');
-  await page.getByPlaceholder('Enter your password').fill('test@admin.com');
+  await page.getByPlaceholder('Enter your email').fill('admin@test.com');
+  await page.getByPlaceholder('Enter your password').fill('admin123');
   await page.getByRole('button', { name: /login/i }).click();
 
   // Wait for redirect
